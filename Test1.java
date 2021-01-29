1042b1b0e9aaeead5044af3348829c64e1b3cee1
Usha Swamy <uswamy@legalzoom.com>
2021-01-29 15:07:00 -0600

    adding new files

---
+++ Test1.java
@@ -0,0 +1,12 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		add(2,5);
+	}
+	
+	public static void add (int x, int y)
+	{
+		int z= x+y;
+		System.out.println(z);
+	}
+}
---
+++ test4.java
@@ -0,0 +1,9 @@
+//https://www.techbeamers.com/java-coding-questions-software-testers/
+public class test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
