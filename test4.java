8b3ab24d47daa106b454897f486112f0737c5708
uswamy520 <78235178+uswamy520@users.noreply.github.com>
2021-01-29 15:38:26 -0600

    Delete test4.java

--- test4.java
+++
@@ -1,9 +0,0 @@
-//https://www.techbeamers.com/java-coding-questions-software-testers/
-public class test4 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
